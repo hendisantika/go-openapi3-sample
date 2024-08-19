0c0bc511464bcab0506e1590b42d301fb3fc184c
hendisantika <hendisantika@yahoo.co.id>
2024-08-19 14:31:07 +0700

    Add blog model🫸🌀✏️📗🐧🐳

---
+++ models/blog.go
@@ -0,0 +1,10 @@
+package models
+
+type Blog struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Body        string `json:"body"`
+	Author      string `json:"author"`
+	IsPublished bool   `json:"isPublished"`
+}
