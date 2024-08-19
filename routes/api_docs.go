851f947ffb7aadca599dd249932f440700af0015
hendisantika <hendisantika@yahoo.co.id>
2024-08-19 14:40:10 +0700

    Add api_docs.go🫸🌀✏️📗🐧🐳

---
+++ routes/api_docs.go
@@ -0,0 +1,15 @@
+// Package routes Blog API.
+//
+//	Schemes: http
+//	BasePath: /
+//	Version: 1.0.0
+//	Host: localhost:8080
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//
+// swagger:meta
+package routes
